ec46f02e8d9b57f42dac84903c5fe11bd86a1ad8
John Bailey <jbaileyi@stanford.edu>
2022-07-05 11:27:35 -0700

    button p2

--- dc1DataVis/app/src/gui/IndividualChannelWindow.py
+++ dc1DataVis/app/src/gui/IndividualChannelWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'IndividualChannelWindow.ui'
+# Form implementation generated from reading ui file 'individualchannelwindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.6
 #
@@ -14,47 +14,69 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_IndividualChannelInfo(object):
     def setupUi(self, IndividualChannelInfo):
         IndividualChannelInfo.setObjectName("IndividualChannelInfo")
-        IndividualChannelInfo.resize(820, 587)
-        self.InputElectrodeNumber = QtWidgets.QTextEdit(IndividualChannelInfo)
-        self.InputElectrodeNumber.setGeometry(QtCore.QRect(100, 0, 71, 31))
-        self.InputElectrodeNumber.setObjectName("InputElectrodeNumber")
+        IndividualChannelInfo.resize(825, 587)
+        self.gridLayout_2 = QtWidgets.QGridLayout(IndividualChannelInfo)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetNoConstraint)
+        self.horizontalLayout.setSpacing(5)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.UpdateInput = QtWidgets.QPushButton(IndividualChannelInfo)
+        self.UpdateInput.setObjectName("UpdateInput")
+        self.horizontalLayout.addWidget(self.UpdateInput)
         self.label = QtWidgets.QLabel(IndividualChannelInfo)
-        self.label.setGeometry(QtCore.QRect(10, 0, 91, 31))
+        self.label.setTextFormat(QtCore.Qt.PlainText)
         self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.InputElectrodeNumber = QtWidgets.QTextEdit(IndividualChannelInfo)
+        self.InputElectrodeNumber.setMaximumSize(QtCore.QSize(167, 167))
+        self.InputElectrodeNumber.setObjectName("InputElectrodeNumber")
+        self.horizontalLayout.addWidget(self.InputElectrodeNumber)
         self.label_2 = QtWidgets.QLabel(IndividualChannelInfo)
-        self.label_2.setGeometry(QtCore.QRect(180, 0, 41, 31))
+        self.label_2.setMaximumSize(QtCore.QSize(50, 167))
         self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(IndividualChannelInfo)
-        self.label_3.setGeometry(QtCore.QRect(270, 0, 31, 31))
-        self.label_3.setObjectName("label_3")
+        self.horizontalLayout.addWidget(self.label_2)
         self.InputElectrodeRow = QtWidgets.QTextEdit(IndividualChannelInfo)
-        self.InputElectrodeRow.setGeometry(QtCore.QRect(220, 0, 41, 31))
+        self.InputElectrodeRow.setMaximumSize(QtCore.QSize(100, 167))
         self.InputElectrodeRow.setObjectName("InputElectrodeRow")
+        self.horizontalLayout.addWidget(self.InputElectrodeRow)
+        self.label_3 = QtWidgets.QLabel(IndividualChannelInfo)
+        self.label_3.setMaximumSize(QtCore.QSize(50, 16777215))
+        self.label_3.setObjectName("label_3")
+        self.horizontalLayout.addWidget(self.label_3)
         self.InputElectrodeCol = QtWidgets.QTextEdit(IndividualChannelInfo)
-        self.InputElectrodeCol.setGeometry(QtCore.QRect(300, 0, 41, 31))
+        self.InputElectrodeCol.setMaximumSize(QtCore.QSize(100, 167))
         self.InputElectrodeCol.setObjectName("InputElectrodeCol")
+        self.horizontalLayout.addWidget(self.InputElectrodeCol)
         self.LabelElectrodeInfo = QtWidgets.QLabel(IndividualChannelInfo)
-        self.LabelElectrodeInfo.setGeometry(QtCore.QRect(350, 0, 461, 31))
+        self.LabelElectrodeInfo.setMaximumSize(QtCore.QSize(167, 167))
         self.LabelElectrodeInfo.setObjectName("LabelElectrodeInfo")
-        self.widget = QtWidgets.QWidget(IndividualChannelInfo)
-        self.widget.setGeometry(QtCore.QRect(11, 40, 641, 361))
-        self.widget.setObjectName("widget")
-        self.gridLayout = QtWidgets.QGridLayout(self.widget)
-        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.addWidget(self.LabelElectrodeInfo)
+        self.totalSamples = QtWidgets.QLabel(IndividualChannelInfo)
+        self.totalSamples.setMaximumSize(QtCore.QSize(167, 167))
+        self.totalSamples.setObjectName("totalSamples")
+        self.horizontalLayout.addWidget(self.totalSamples)
+        self.gridLayout_2.addLayout(self.horizontalLayout, 0, 0, 1, 1)
+        self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
-        self.AmplitudeHistPlot = PlotWidget(self.widget)
-        self.AmplitudeHistPlot.setObjectName("AmplitudeHistPlot")
-        self.gridLayout.addWidget(self.AmplitudeHistPlot, 0, 0, 1, 1)
-        self.ChannelTracePlot = PlotWidget(self.widget)
+        self.ChannelTracePlot = PlotWidget(IndividualChannelInfo)
+        self.ChannelTracePlot.setMinimumSize(QtCore.QSize(402, 247))
         self.ChannelTracePlot.setObjectName("ChannelTracePlot")
         self.gridLayout.addWidget(self.ChannelTracePlot, 0, 1, 1, 1)
-        self.SpikeRateHistPlot = PlotWidget(self.widget)
-        self.SpikeRateHistPlot.setObjectName("SpikeRateHistPlot")
-        self.gridLayout.addWidget(self.SpikeRateHistPlot, 1, 0, 1, 1)
-        self.SpikeRatePlot = PlotWidget(self.widget)
+        self.AmplitudeHistPlot = PlotWidget(IndividualChannelInfo)
+        self.AmplitudeHistPlot.setMinimumSize(QtCore.QSize(402, 247))
+        self.AmplitudeHistPlot.setObjectName("AmplitudeHistPlot")
+        self.gridLayout.addWidget(self.AmplitudeHistPlot, 0, 0, 1, 1)
+        self.SpikeRatePlot = PlotWidget(IndividualChannelInfo)
         self.SpikeRatePlot.setEnabled(True)
+        self.SpikeRatePlot.setMinimumSize(QtCore.QSize(402, 247))
         self.SpikeRatePlot.setObjectName("SpikeRatePlot")
         self.gridLayout.addWidget(self.SpikeRatePlot, 1, 1, 1, 1)
+        self.SpikeRateHistPlot = PlotWidget(IndividualChannelInfo)
+        self.SpikeRateHistPlot.setMinimumSize(QtCore.QSize(402, 247))
+        self.SpikeRateHistPlot.setObjectName("SpikeRateHistPlot")
+        self.gridLayout.addWidget(self.SpikeRateHistPlot, 1, 0, 1, 1)
+        self.gridLayout_2.addLayout(self.gridLayout, 1, 0, 1, 1)
         self.label.setBuddy(self.InputElectrodeNumber)
         self.label_2.setBuddy(self.InputElectrodeRow)
         self.label_3.setBuddy(self.InputElectrodeCol)
@@ -65,8 +87,25 @@ class Ui_IndividualChannelInfo(object):
     def retranslateUi(self, IndividualChannelInfo):
         _translate = QtCore.QCoreApplication.translate
         IndividualChannelInfo.setWindowTitle(_translate("IndividualChannelInfo", "Individual Channel Information"))
-        self.label.setText(_translate("IndividualChannelInfo", "Electrode No."))
+        self.UpdateInput.setText(_translate("IndividualChannelInfo", "Update Input"))
+        self.label.setText(_translate("IndividualChannelInfo", "Electrode No. "))
+        self.InputElectrodeNumber.setHtml(_translate("IndividualChannelInfo", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Segoe UI\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
         self.label_2.setText(_translate("IndividualChannelInfo", "Row"))
+        self.InputElectrodeRow.setHtml(_translate("IndividualChannelInfo", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Segoe UI\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
         self.label_3.setText(_translate("IndividualChannelInfo", "Col"))
-        self.LabelElectrodeInfo.setText(_translate("IndividualChannelInfo", "Extra information"))
+        self.InputElectrodeCol.setHtml(_translate("IndividualChannelInfo", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'Segoe UI\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
+        self.LabelElectrodeInfo.setText(_translate("IndividualChannelInfo", "Extra information:"))
+        self.totalSamples.setText(_translate("IndividualChannelInfo", "TextLabel"))
 from pyqtgraph import PlotWidget
